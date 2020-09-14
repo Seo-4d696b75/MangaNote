685939de7711723365b6243df224750b440eb278
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-09-14 12:05:15 +0900

    Init

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import Flask, jsonify
+from flask_cors import CORS
+from .database import init_db
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config.from_object('app.config.Config')
+CORS(app)
+
+
+init_db(app)
+
+
+@app.route('/')
+def index():
+    app.logger.warn('warn')
+    app.logger.error('error')
+    app.logger.critical('critical')
+    return jsonify({
+        "message": "test"
+    })
+
+
+if __name__ == '__main__':
+    app.run()
