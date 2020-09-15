36b1fdc4abc6a9d101ff5316d8a155f5d34f60cc
lasurstein <sabamoffy.1258@gmail.com>
2020-09-15 13:49:45 +0900

    いいね登録/削除APIの追加

--- backend/blueprints/likes.py
+++ backend/blueprints/likes.py
@@ -19,3 +19,39 @@ def index(book_id, comment_id):
         "comment_id": comment_id,
         "message": "likes_test"
     })
+
+@likes.route('/likes/<int:user_id>',  methods=['PUT'])
+def put_likes(book_id, comment_id, user_id):
+    exist_likes = db.session.query(Like).filter(Like.user_id == user_id, Like.comment_id == comment_id).one()
+    if exist_likes is not None:
+        return jsonify({"status": "Bad Request", "message": "The user already liked this comment."}), 400
+    
+    try:
+        (user_name, ) = db.session.query(User.name).filter(User.id == user_id).one()
+    except:
+        return jsonify({"status": "Not Found", "message": "The user not found."}), 404
+
+    like = Like(user_id, comment_id, user_name, user_name)
+    db.session.add(like)
+
+    try:
+        db.session.commit()
+    except:
+        return jsonify({"status": "Internal Sever Error"}), 503
+
+    return None, 204
+
+@likes.route('/likes/<int:user_id>',  methods=['DELETE'])
+def delete_likes(book_id, comment_id, user_id):
+    exist_likes = db.session.query(Like).filter(Like.user_id == user_id, Like.comment_id == comment_id).one()
+    if exist_likes is None:
+        return jsonify({"status": "Bad Request", "message": "The user hasn't liked this comment."}), 400
+    else:
+        db.session.delete(exist_likes)
+
+    try:
+        db.session.commit()
+    except:
+        return jsonify({"status": "Internal Sever Error"}), 503
+
+    return None, 204
