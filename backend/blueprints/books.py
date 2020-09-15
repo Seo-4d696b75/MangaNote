8f02a9d77c36dea002d3242afc74047689875c08
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-09-15 10:20:12 +0900

    マンガ画像をbase64encodeされた画像の配列として返すエンドポイントを作成

--- backend/blueprints/books.py
+++ backend/blueprints/books.py
@@ -2,6 +2,8 @@ from flask import jsonify, Blueprint
 import logging
 from app.database import db
 from app.models import *
+import os
+import glob
 
 import base64
 
@@ -16,4 +18,40 @@ def index():
     logger.critical('critical')
     return jsonify({
         "message": "books_test"
+    })
+
+
+@books.route('/<book_id>')
+def get_detail(book_id):
+    try:
+        book = Book.query.get(book_id)
+    except Exception as e:
+        logger.error(e)
+        return jsonify({"status": "Internal server error"}), 500
+
+    if book is None:
+        return jsonify({"status": "Not Found"}), 404
+
+    images = []
+    paths = glob.glob(book.path + '*')
+    paths.sort()
+    for path in paths:
+        extention = path.split('.', 1)[1]
+        ret = f"data:image/{extention};base64,"
+        with open(path, "rb") as f:
+            try:
+                img_binary = f.read()
+            except Exception as e:
+                logger.error(e)
+                return jsonify({"status": "Internal server error"}), 500
+
+            b64_binary = base64.b64encode(img_binary)
+            b64_string = b64_binary.decode('utf-8')
+            ret += b64_string
+            images.append(ret)
+
+    return jsonify({
+        "id": book_id,
+        "title": book.title,
+        "images": images,
     })
