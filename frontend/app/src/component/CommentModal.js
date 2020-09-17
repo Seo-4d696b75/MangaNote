3b3c504f3c72e16012e0eacde80a1009522cff9a
lasurstein <sabamoffy.1258@gmail.com>
2020-09-18 05:54:20 +0900

    コメント入力モーダル

--- frontend/app/src/component/CommentModal.js
+++ frontend/app/src/component/CommentModal.js
@@ -1,14 +1,14 @@
 import React, {useState} from "react";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import InputGroup from 'react-bootstrap/InputGroup';
+import {Modal, Button, Form, Image, InputGroup, ToggleButton, ButtonGroup} from 'react-bootstrap';
+
+import "../styles/sass/component/CommentModal.scss";
+import comment_edit_svg from "../images/icon/comment_edit.svg";
+import pin_edit_svg from "../images/icon/pin_edit.svg";
 
 function CommentModal({show, handleClose, appendComment}) {
   const [text, setText] = useState("");
   const [activeTab, setActiveTab] = useState('comment');
+  const [checked, setChecked] = useState(false);
 
   const handleClick = () => {
     let type;
@@ -27,40 +27,72 @@ function CommentModal({show, handleClose, appendComment}) {
 
   return (
     <div>
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Body>
-          <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)}>
-            <Tab eventKey="comment" title="コメント">
-              <Form.Group>
-                <Form.Control
-                  placeholder="コメントを入力してください"
-                  as="textarea"
-                  rows="3"
-                  onChange={event => {setText(event.target.value)}}
-                />
-                <InputGroup className="mb-3">
-                  <InputGroup.Prepend>
-                    <InputGroup.Checkbox id="isSpoiler" />
-                  </InputGroup.Prepend>
-                  <span>ネタバレコメントに設定する</span>
-                </InputGroup>
-              </Form.Group>
-            </Tab>
-            <Tab eventKey="map" title="聖地">
-            </Tab>
-          </Tabs>
+      <Modal show={show} onHide={handleClose} centered id="edit">
+        <div id="tabs">
+          <Button eventKey="comment" className="tab--active">
+            <Image
+              src={comment_edit_svg}
+              roundedCircle
+              className="icon--active"
+            />
+            <span className="title--active">コメント</span>
+          </Button>
+          <Button className="tab">
+            <Image src={pin_edit_svg} roundedCircle className="icon"/>
+            <span className="title">聖 地</span>
+          </Button>
+        </div>
+        <Modal.Body className="body">
+          <Form.Group>
+            <Form.Control
+              placeholder="コメントを入力してください"
+              as="textarea"
+              rows="3"
+              onChange={(event) => {
+                setText(event.target.value);
+              }}
+              className="comment-area"
+            />
+            <InputGroup id="netabare" className="mb-3">
+              <InputGroup.Prepend className="wrapper">
+                <InputGroup.Checkbox id="isSpoiler" checked={checked} />
+              </InputGroup.Prepend>
+            </InputGroup>
+            <div>
+              <ButtonGroup toggle className="mb-2">
+                <ToggleButton
+                  type="checkbox"
+                  variant="secondary"
+                  checked={checked}
+                  value="1"
+                  id="isNetabare"
+                  onChange={(e) => setChecked(e.currentTarget.checked)}
+                  className={`toggle${checked ? "--active" : ""}`}
+                ></ToggleButton>
+              </ButtonGroup>
+              <span>ネタバレコメントに設定する</span>
+            </div>
+          </Form.Group>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Modal.Footer className="footer">
+          <Button
+            variant="secondary"
+            onClick={handleClose}
+            className="btn-cancel"
+          >
             キャンセル
           </Button>
-          <Button variant="primary" onClick={handleClick}>
-            投稿
+          <Button
+            variant="primary"
+            onClick={handleClick}
+            className="btn-post"
+          >
+            投 稿
           </Button>
         </Modal.Footer>
       </Modal>
     </div>
-  )
+  );
 }
 
 export default CommentModal;
